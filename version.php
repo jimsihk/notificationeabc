ab2abcc4ecc3d37d8dd213116c2b1c053b0fb0bb
jimsihk <99048231+jimsihk@users.noreply.github.com>
2024-02-12 23:22:42 +0800

    Publish release

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024021200;
+$plugin->version   = 2024021201;
 $plugin->requires  = 2016120503;
 $plugin->component = 'enrol_notificationeabc';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.2.0.1-forked-1.0.0';
+$plugin->release = '3.2.0.1-forked-1.0.1';
 $plugin->cron      = 30;
